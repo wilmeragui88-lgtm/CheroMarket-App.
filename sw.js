36b9c300a5bc203814561a3f4eee4cd75f420e2d
CheroMarket <wilmeragui.88@icloud.com>
2026-02-03 21:03:54 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,20 @@
-// Dentro de sw.js
+const CACHE_NAME = 'cheromarket-v1';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png', // Asegúrate que aquí diga .png
-  './icon-512.png'  // Asegúrate que aquí diga .png
+  './icon-192.png',
+  './icon-512.png'
 ];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
